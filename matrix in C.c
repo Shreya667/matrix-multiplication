6352ba0e53b0fa59aa349a59865dbb1af0158e0f
shivamy221 <shivamy221@gmail.com>
2020-03-03 23:38:27 +0530

    Rectified all the errors and formatted code

--- matrix in C.c
+++ matrix in C.c
@@ -1,69 +1,71 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>                                                             //Added so can use any system command(command use in
+                                                                                // command prompt) in this case "cls" used to clear screen
 int main()
 {
-    int r1,c1,r2,c2,i,j,k,a,b;
-    int first[10][10],second[10][10],multiply[10][10];
+    int r1,c1,r2,c2;                                                            //No of row and columns variable for both matrix.
+    int i,j,k;                                                                  //iteration variables.
+    int first[10][10],second[10][10],multiply[10][10];                          //Matrix variables.
 
-    printf("enter number of rows and columns of first matrix");
+    printf("enter number of rows and columns of first matrix : ");
     scanf("%d%d", &r1, &c1);
 
-    printf("enter number of rows and columns of second matrix");
+    printf("enter number of rows and columns of second matrix : ");
     scanf("%d%d", &r2,&c2 );
 
     while( c1!= r2)
         {
+            system("cls");
+            printf("\n Matrix is possible only if number of columns in \n first matrix is same as number of rows in second matrix\n\n");
+            printf("Enter rows and columns of first matrix : ");
+            scanf("%d%d",&r1,&c1);
+            printf("Enter rows and columns of second matrix : ");
+            scanf("%d%d",&r2,&c2);
+        }
 
-        printf("no Multiplication");
-        printf("enter rows and columns of first matrix");
-        scanf("%d%d",&r1,&c1);
-        printf("enter rows and columns of second matrix");
-        scanf("%d%d",&r2,&c2);
-
-    }
-    printf("enter elements of matrix");
+    system("cls");                                                              //cls is used to clear screen.
+    printf("Enter elements of First matrix : \n\n");                            //if you don't know what \n is read about escape sequences in C.
     for(i=0; i<r1; i++)
-    for(j=0; j<c1; j++)
-    {
-        printf("enter elements a %d%d",i+1,j+1);
-        scanf("%d", &a [i][j]);
+        for(j=0; j<c1; j++)
+            {
+                printf("Enter element at place %d, %d : ",i+1,j+1);
+                scanf("%d", &first[i][j]);                                      //2D array defined above was not taken hence error.
+            }
 
-    }
-    printf("enter elements of second matrix");
+    system("cls");
+    printf("Enter elements of second matrix : \n\n");
     for(i=0; i<r2; i++)
-    for(j=0; j<c2; j++)
-    {
-        printf("enter elements b %d%d",i+1,j+1);
-        scanf("%d", &b [i][j]);
-    }
-
+        for(j=0; j<c2; j++)
+            {
+                printf("Enter elements b %d, %d : ",i+1,j+1);
+                scanf("%d", &second[i][j]);
+            }
     for(i=0; i<r1; i++)
-    for(j=-; j<c2; j++)
-    {
-        multiply[i][j]=0;
-    }
-
+        {
+            for(j=0; j<c2; j++)                                                 //j=- gives error, valid value must be assigned.
+            {
+                multiply[i][j]=0;
+            }
+        }
     for(i=0; i<r1; i++)
-    for(j=0; j<c2; j++)
-    for(k=0; k<c1; k++)
+        for(j=0; j<c2; j++)
+            for(k=0; k<c1; k++)
+                {
+                    multiply[i][j]+=first[i][k]*second[k][j];                   //Again a & b were taken instead of defined array.
+                }
 
-    {
+    system("cls")
+    printf("\n\n\n\n output matrix : ");
+    for(i=0; i<r1; i++)                                                         //Did a little formating so output is bit clean.
+       {
+            printf("\n   ");
+            for(j=0; j<c2; j++)
+                printf("%d ",multiply[i][j]);
+       }
 
-        multiply[i][j]+=a[i][k]*b[k][j];
-    }
-    printf("output matrix");
-    for(i=0; i<r1; i++)
-    for(j=0; j<c2; j++)
-    {
+    printf("\n\n\n\n");
+    return 0;
 
-        printf("%d",multiply[i][j]);
-        if(j==c2-1)
-            printf("\n\n");
-
-    }
-
-return 0;
 }
 
 
